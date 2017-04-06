37913d1d5402f24c20e59e6ab10c9947fd7fad74
narcolic <narcolic832@gmail.com>
2017-04-06 19:34:05 +0100

    contact form

--- src/AppBundle/Controller/fypController.php
+++ src/AppBundle/Controller/fypController.php
@@ -94,9 +94,54 @@ class fypController extends Controller
      */
     public function aboutAction(Request $request)
     {
+        $form = $this->createForm('Acme\Bundle\Type\ContactType',null,array(
+            // To set the action use $this->generateUrl('route_identifier')
+            'method' => 'POST'
+        ));
 
-        // replace this example code with whatever you need
-        return $this->render('mainpage/about.html.twig');
+        if ($request->isMethod('POST')) {
+            // Refill the fields in case the form is not valid.
+            $form->handleRequest($request);
+
+            if($form->isValid()){
+                // Send mail
+                if($this->sendEmail($form->getData())){
+
+                    // Everything OK, redirect to wherever you want ! :
+
+                    return $this->redirectToRoute('about');
+                }else{
+                    // An error ocurred, handle
+                    var_dump("Errooooor :(");
+                }
+            }
+        }
+
+        return $this->render('mainpage/about.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+
+    private function sendEmail($data){
+        $myappContactMail = 'narcolic832@gmail.com';
+        $myappContactPassword = 'Onionada832@';
+
+        // In this case we'll use the ZOHO mail services.
+        // If your service is another, then read the following article to know which smpt code to use and which port
+        // http://ourcodeworld.com/articles/read/14/swiftmailer-send-mails-from-php-easily-and-effortlessly
+        $transport = \Swift_SmtpTransport::newInstance('smtp.gmail.com', 465,'ssl')
+            ->setUsername($myappContactMail)
+            ->setPassword($myappContactPassword);
+
+        $mailer = \Swift_Mailer::newInstance($transport);
+        $message = \Swift_Message::newInstance("Our Code World Contact Form ". $data["subject"])
+            ->setFrom(array($myappContactMail => "Message by ".$data["name"]))
+            ->setTo(array($myappContactMail => $myappContactMail))
+            ->setBody($data["message"]."<br>ContactMail :".$data["email"]);
+        $result = $mailer->send($message);
+
+
+        return $result;
     }
 
     /**
